be63ebf116c9c11a6a6e94f73446b70a6d98228b
divesh kumar <divesh.kumar2905@gmail.com>
2024-10-19 13:34:49 +1200

    i have added changes to my accounts.html page and the some of the css has been changed in the frame.css file

--- Nulook/Nulook/settings.py
+++ Nulook/Nulook/settings.py
@@ -110,7 +110,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images for icons)
+# Static files (CSS, JavaScript, Images for icons).
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
